2eb5324180f9dc3f99a7d9076c31274b8bb9e91d
Sebastián Miranda <sebastian.mirandaramirez@ucr.ac.cr>
2025-10-08 19:15:44 -0600

    Commit inicial: se añade README, gitignore y el primer script para manipular data

---
+++ README.md
@@ -0,0 +1,16 @@
+# Proyecto Grupal CA0204
+
+## Integrantes 
+
+- Sebastián Calderón Segura C21517
+- Sebastián Miranda Ramírez C4H274
+- Josué Gustavo Rodríguez Aguilar C4J023
+- Kevin David Calderon Martínez C4D511
+- Santiago Paniagua Chavarría C4I249
+
+## Requisitos
+
+Instalar los paquetes
+
+-tidyverse
+-here
---
+++ scripts/01_unir_estadisticas.R
@@ -0,0 +1,35 @@
+library(readxl)
+library(tidyverse)
+library(here)
+
+lista.datos <- list()
+
+for(anio in 19:25){
+  
+  extension <- ifelse(anio %in% c(19:23), ".xlsx", ".xls")
+  nombre.archivo <- paste0("Estadísticas Policiales 20", anio, extension)
+  
+  ruta <- here("data_raw", "estadisticaspoliciales", nombre.archivo)
+  
+  if (!file.exists(ruta)) {
+    message("Falta", ruta, ". Saltando este año.")
+    next
+  }
+  
+  temp <- read_excel(ruta)
+  names(temp) <- tolower(names(temp))
+  temp$fecha <- as.Date(temp$fecha, format = "%Y-%m-%d")
+  
+  if ("genero" %in% names(temp) && !"sexo" %in% names(temp)) {
+    temp <- rename(temp, sexo = genero)
+  }
+  
+  lista.datos[[length(lista.datos) + 1]] <- temp
+}
+
+datos.2019.2025 <- bind_rows(lista.datos)
+
+ruta.salida <- here("data", "processed", "Estadísticas Policiales 2019 a Julio 2025.csv")
+
+
+write_csv(datos.2019.2025, ruta.salida)
